800c299f3db23715417400237ec726afb435fbe9
Karamchi <karamchi.ghandi@gmail.com>
2015-10-28 20:17:36 -0300

    Script andando de tracert

--- tp2/trace.py
+++ tp2/trace.py
@@ -4,13 +4,35 @@ logging.getLogger("scapy").setLevel(1)
 
 from scapy.all import *
 
-h = "www.dc.uba.ar"
+h = "mit.edu"
+ansv=[]
+last_src = None
 
-for ttl in range(65):
-	print ttl
+for ttl in range(1,65):
+	
 	pk = IP(dst=h, ttl = ttl) / ICMP()
-	ans= sr(pk, timeout = 5)
+	ans, unans= sr(pk, timeout = 5, verbose=False)
 	#if(len(ans) > 0):
-	print(ans)
+	
+#	for i in unans:
+#		print i
+        if len(ans) == 0:
+               print ttl, "***"
+	else:
+		snd, rcv = ans[0]
+		snd.show()
+		if last_src == rcv.src:
+			break
+		last_src = rcv.src
+		print ttl, rcv.src
+		
+		
+   
+
+#for i in range(0, 11):
+#	for snd, rcv in ansv[i]:
+#		print i+1, rcv.src
+#	if len(ansv[i]) == 0:
+ #               print i+1, "*"
 	# else:
 	# 	print("timeout")
