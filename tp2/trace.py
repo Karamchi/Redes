4e1247146a6f7f4e7ef445a2d9705c9ed279b334
Karamchi <karamchi.ghandi@gmail.com>
2015-10-31 18:54:06 -0300

    Herramienta

--- tp2/trace.py
+++ tp2/trace.py
@@ -1,38 +1,31 @@
 #! /usr/bin/env python
 import logging
+import sys
 logging.getLogger("scapy").setLevel(1)
 
 from scapy.all import *
 
-h = "mit.edu"
-ansv=[]
+h = sys.argv[1]
 last_src = None
+lines=[]
+mediciones=[[]]*65
 
-for ttl in range(1,65):
-	
-	pk = IP(dst=h, ttl = ttl) / ICMP()
-	ans, unans= sr(pk, timeout = 5, verbose=False)
-	#if(len(ans) > 0):
-	
-#	for i in unans:
-#		print i
-        if len(ans) == 0:
-               print ttl, "***"
-	else:
-		snd, rcv = ans[0]
-		snd.show()
-		if last_src == rcv.src:
-			break
-		last_src = rcv.src
-		print ttl, rcv.src
-		
-		
-   
+for i in range(1,100):
 
-#for i in range(0, 11):
-#	for snd, rcv in ansv[i]:
-#		print i+1, rcv.src
-#	if len(ansv[i]) == 0:
- #               print i+1, "*"
-	# else:
-	# 	print("timeout")
+	lines = []
+
+	for ttl in range(1,65):
+		
+		pk = IP(dst=h, ttl = ttl) / ICMP()		#Arma paquete
+		ans, unans= sr(pk, timeout = 5, verbose=False)	#Manda paquete
+		if(len(ans) > 0):
+			snd, rcv = ans[0]
+			if last_src == rcv.src:	break
+			last_src = rcv.src
+			mediciones[ttl].append((rcv.time-snd.time)*2)
+			lines.append([ttl, rcv.src, sum(mediciones[ttl])/len(mediciones[ttl])])
+
+	print lines[0][0], lines[0][1] , '\t', lines[0][2]
+	for i in range(1,len(lines)): 
+		a = lines[i][2] - lines[i-1][2]
+		print lines[i][0], lines[i][1], '\t', a if a>0 else 0
