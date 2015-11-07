2537a22ec78150b8e3fbac71e6ee18bb4380dbb2
Marco Vanotti <mvanotti@dc.uba.ar>
2015-11-07 12:33:35 -0300

    Cambios con DK y Spinetta en Bonafide
    Se agregó un mapa de tuplas (host1, host2) para identificar los saltos
    entre el host1 y el host2 (basicamente cada nuevo paquete se pone en el
    hash table con el host2 que respondió y el host1 siendo el inmediato
    anterior). Los valores del hash table son los rtts entre estos dos
    hosts, o 0 si es negativo. Finalmente, calcular el rtt entre ambos hosts
    es simplemente tomar el promedio de los rtts calculados.

--- tp2/trace.py
+++ tp2/trace.py
@@ -6,39 +6,57 @@ import math
 logging.getLogger("scapy").setLevel(1)
 
 from scapy.all import *
-from scipy import stats as st
+#from scipy import stats as st
 import csv
 
-if (len(sys.argv)!=4): print "Uso: iteraciones destino salida"
-else: 
-	h = sys.argv[2]
-	last_src = None
-	rtt=[[]]*65
-	salida = open(sys.argv[3]+".csv","w")
-	csvsalida = csv.writer(salida)
 
-	for i in range(int(sys.argv[1])):
+def main():
+    if (len(sys.argv)!=4): 
+        print "Uso: iteraciones destino salida"
+        return
 
-		lines = []
-	#	drtt=[]
+    host = sys.argv[2]
+    rtt = [[]] * 65
+    salida = open(sys.argv[3] + ".csv", "w")
+    csvsalida = csv.writer(salida)
+    rtts = {}
 
-		for ttl in range(1,65):
-		
-			pk = IP(dst=h, ttl = ttl) / ICMP()
-			ans, unans= sr(pk, timeout = 5, verbose=False)
-			if(len(ans) > 0):
-				snd, rcv = ans[0]
-				if last_src == rcv.src:	break
-				last_src = rcv.src
-				time=(rcv.time-snd.time)*2
-				rtt[ttl].append(time)
-				csvsalida.writerow((ttl, rcv.src, time))
-				lines.append([ttl, rcv.src, sum(rtt[ttl])/len(rtt[ttl]), numpy.std(rtt[ttl])])
+    for z in xrange(int(sys.argv[1])):
+        lines = []
+        last_src = "127.0.0.1"
+        last_rtt = 0
+        for ttl in range(1, 65):
+            pk = IP(dst = host, ttl = ttl) / ICMP()
+            ans, unans = sr(pk, timeout = 3, verbose=False)
+            if(len(ans) == 0):
+                # No obtuve respuesta. Que hacemos?
+                print "Paquete salteado. TTL: ", ttl
+                continue
+            snd, rcv = ans[0]
+            if last_src == rcv.src:
+                break
+            time = rcv.time - snd.time 
+            if (last_src, rcv.src) not in rtts:
+                rtts[(last_src, rcv.src)] = []
+            rtts[(last_src, rcv.src)].append(max(time - last_rtt, 0))
+            print "Agregando (%s -> %s) con RTT: %.4f" % (last_src, rcv.src, time - last_rtt)
+            last_src = rcv.src
+            last_rtt = time
+            rtt[ttl].append(time)
+            csvsalida.writerow((ttl, rcv.src, time))
+            lines.append([ttl, rcv.src, sum(rtt[ttl])/len(rtt[ttl]), numpy.std(rtt[ttl])])
+        
+        print "%d %s\t%.2f %.2f" % (lines[0][0], lines[0][1] , lines[0][2], lines[0][3])
+        for i in range(1,len(lines)): 
+            a = lines[i][2] - lines[i-1][2]
+            a = 0 if a < 0 else a
+            print "%d %s\t%.2f %.2f" % (lines[i][0], lines[i][1], a, lines[i][3])
+        print " " 
+    for (p, q) in rtts:
+        print "(%s -> %s): \n\t" % (p, q),
+        for r in rtts[(p, q)]:
+            print " %.4f " % r,
+        print "\n\t %.5f" % (sum(rtts[(p,q)])/len(rtts[(p,q)]))
 
-		print lines[0][0], lines[0][1] , '\t', lines[0][2], lines[0][3] #Soy un cabeza
-		for i in range(1,len(lines)): 
-			a = lines[i][2] - lines[i-1][2]
-			a = 0 if a<0 else 0
-	#		drtt.append(a)
-			print lines[i][0], lines[i][1], '\t', a, lines[i][3]
-		print " " 
+if __name__ == "__main__":
+    main()
