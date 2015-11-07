863ae9f9192bb4e08770185c05c9309c153f3c6b
Marco Vanotti <mvanotti@dc.uba.ar>
2015-11-07 12:51:49 -0300

    Ahora los paquetes se mandan todos de una.
    Con este cambio hacemos que los 65 paquetes se manden de una, y luego
    ordenamos las respuestas por TTL ascendente y nos quedamos con RTTs
    entre los paquetes i e i+1.
    
    Este cambio ignora el TTL entre nuestra pc y el host 0, pero se puede
    agregar trivialmente.
    
    REMOVI TODO LO QUE RESPECTA A TRACEROUTE. Lo podemos agregar después sin
    mucho problema.

--- tp2/trace.py
+++ tp2/trace.py
@@ -22,36 +22,27 @@ def main():
     rtts = {}
 
     for z in xrange(int(sys.argv[1])):
-        lines = []
-        last_src = "127.0.0.1"
-        last_rtt = 0
-        for ttl in range(1, 65):
-            pk = IP(dst = host, ttl = ttl) / ICMP()
-            ans, unans = sr(pk, timeout = 3, verbose=False)
-            if(len(ans) == 0):
-                # No obtuve respuesta. Que hacemos?
-                print "Paquete salteado. TTL: ", ttl
-                continue
-            snd, rcv = ans[0]
-            if last_src == rcv.src:
-                break
-            time = rcv.time - snd.time 
-            if (last_src, rcv.src) not in rtts:
-                rtts[(last_src, rcv.src)] = []
-            rtts[(last_src, rcv.src)].append(max(time - last_rtt, 0))
-            print "Agregando (%s -> %s) con RTT: %.4f" % (last_src, rcv.src, time - last_rtt)
-            last_src = rcv.src
-            last_rtt = time
-            rtt[ttl].append(time)
-            csvsalida.writerow((ttl, rcv.src, time))
-            lines.append([ttl, rcv.src, sum(rtt[ttl])/len(rtt[ttl]), numpy.std(rtt[ttl])])
+        pk = IP(dst = host, ttl = range(1, 65)) / ICMP()
+        ans, unans = sr(pk, timeout = 10, verbose=False)
+        # Ignoramos los unanswered.
+        print "Perdidos: %d" % (len(unans))
+        sorted(ans, key=lambda pkt: pkt[0].ttl)
         
-        print "%d %s\t%.2f %.2f" % (lines[0][0], lines[0][1] , lines[0][2], lines[0][3])
-        for i in range(1,len(lines)): 
-            a = lines[i][2] - lines[i-1][2]
-            a = 0 if a < 0 else a
-            print "%d %s\t%.2f %.2f" % (lines[i][0], lines[i][1], a, lines[i][3])
-        print " " 
+        # Ahora tenemos los ans ordenados por TTL
+        # Tenemos que armar la hash table que queriamos.
+        # Para eso, queremos juntar el paquete i con el paquete i+1
+        for i in xrange(0, len(ans) - 1):
+            psnd, prcv = ans[i]
+            snd, rcv = ans[i + 1]
+            if rcv.src == prcv.src:
+                print "Breaking for"
+                break
+            time = max((rcv.time - snd.time) - (prcv.time - psnd.time), 0.0)
+            if (prcv.src, rcv.src) not in rtts:
+                rtts[(prcv.src, rcv.src)] = []
+            rtts[(prcv.src, rcv.src)].append(time)
+            print "Agregando (%s -> %s) con RTT: %.4f\t TTLs: (%d, %d)" % (prcv.src, rcv.src, time, psnd.ttl, snd.ttl)
+        
     for (p, q) in rtts:
         print "(%s -> %s): \n\t" % (p, q),
         for r in rtts[(p, q)]:
