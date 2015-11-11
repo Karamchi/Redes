ce82ae9acab547e3c80a48b7e57338ef4ceb5393
Karamchi <karamchi.ghandi@gmail.com>
2015-11-11 19:08:27 -0300

    Para tenerlos

--- tp2/tests.py
+++ tp2/tests.py
@@ -4,48 +4,49 @@ import sys
 import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
+import analysis
+import math
 
-ds = {}
+f = "results/"+sys.argv[1]
+res = analysis.parsefile(f)
+res = analysis.process3(res)
 
-with open(str(sys.argv[1]), 'r') as input_file:
-	while input_file:
-		ips = input_file.readline()
-		if not ips:
-			break
-		ips = [i.replace("\n", "") for i in ips.split(" ")]
-		ips = [i for i in ips if i != ""]
+#print res
 
-		ttls = [i.replace("\n", "") for i in input_file.readline().split(" ")]
-		ttls = [float(i) for i in ttls if i != ""]
-		
-		if len(ttls) >= 5:
-			ds[np.average(ttls)] = ips 
-			# print(ips)
-			# print len(ttls), np.average(ttls), np.std(ttls), "\n"
-		
-		d = input_file.readline()
+ds = [math.log(a[0],2) for a in res]
+hosts = [a[1] for a in res]
+#ds = [a[0] for a in res]
 
-# print ds
-print "N\t\t\t= %d" % len(ds)
-print "Mean\t\t\t= %f" % np.average(ds.keys())
-print "Standard deviation\t= %f" % np.std(ds.keys())
-print "Normal test\t\t=",  stats.normaltest(ds.keys())
-plt.hist(ds.keys(), bins=200)
+print ds
+print hosts
+
+#print "\n",
+#print "N\t\t\t= %d" % len(ds)
+#print "Mean\t\t\t= %f" % np.average(ds)
+#print "Standard deviation\t= %f" % np.std(ds)
+#print "Normal test\t\t=",  stats.normaltest(ds)
+#plt.hist(ds, bins=200)
 #plt.show()
+print "\n",
 
-
-#Queremos ver si el mayor elemento es un outlier:
+#Grubbs:
 
 while True:
-	n = float(len(ds.keys()))
-	
-	G = max([(y_i - np.average(ds.keys()))/np.std(ds.keys()) for y_i in ds.keys()])
-	alpha = .5
-	t = stats.t.isf(alpha/n,n-2)
-	print "T = %f" % t
+	n = float(len(ds))
+#	print ds
+
+	G = max([(y_i - np.average(ds))/np.std(ds) for y_i in ds])
+	alpha = .05
+	t = (stats.t.isf(alpha/n,n-2))
+	#print "T = %f" % t
 	threshold = (n-1)/n**.5 * (t/(n-2+t))**.5
-	if G > t:
-		print "Outlier:", max(ds.keys()) , ds[max(ds.keys())]
-		del ds[max(ds.keys())]
+	if G > threshold:
+		print ds.index(max(ds))	
+		ant = hosts[ds.index(max(ds))-1]
+		h = hosts.pop(ds.index(max(ds)))
+		print "Outlier:", ant, "->", h , max(ds)
+		ds.remove(max(ds))
 	else:
 		break
+
+print "\n",
